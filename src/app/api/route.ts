3558928f68c52cbcd3da34ef57440f491f753b40
cjlangan <connorjameslangan@gmail.com>
2025-05-21 11:41:16 -0500

    wip: added structure for database, types, scripts, jobs

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,25 +1,29 @@
 import { NextResponse } from 'next/server';
 
 const API_KEY = process.env.GOOGLE_API_KEY;
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const YOUTUBE_API = 'https://youtube.googleapis.com/youtube/v3/';
+const ADDONS = 'order=date&maxResults=1&type=video';
+// const CLIENT_ID = process.env.CLIENT_ID;
+// const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 export async function GET() {
     try {
         const latestVideoRes = await fetch(
-            `${YOUTUBE_API}/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet&order=date&maxResults=1&type=video`
+            `${YOUTUBE_API}/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet&${ADDONS}`
         );
         const latestVideoData = await latestVideoRes.json();
 
-        console.log(latestVideoData);
 
         const videoId = latestVideoData.items?.[0]?.id?.videoId;
+
 
         if(!videoId) {
             return NextResponse.json({ error: 'No video found' }, { status: 404 });
         }
+
+        console.log(latestVideoData);
+        console.log(videoId);
 
         return NextResponse.json({ videoId });
         
---
+++ src/types/Snippet.ts
@@ -0,0 +1,11 @@
+export type Snippet = {
+    text: string;
+    overlapping_text: string;
+    start: string;
+    end: string;
+    title: string;
+    image: string;
+    vid_id: string;
+    id: string;
+    premium: boolean;
+}
