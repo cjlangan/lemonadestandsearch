131c9e32ccf0f0c3603ebc804f9244e8a1be64bc
cjlangan <connorjameslangan@gmail.com>
2025-05-28 21:39:38 -0500

    feat: Typescript script made to get captions from podcast

--- src/lib/get_latest_captions.ts
+++ src/lib/get_latest_captions.ts
@@ -1,19 +1,31 @@
+import fs from 'fs';
 import YTDlpWrap from 'yt-dlp-wrap';
-import { createWriteStream } from 'fs';
 
 const ytDlpWrap = new YTDlpWrap('./yt-dlp');
 
-async function downloadVideo(url: string, outputFile: string) {
+function downloadCaptions(url: string, filename: string) {
     const stream = ytDlpWrap.execStream([
         url,
-        '-f', 'best[ext=mp4]',
-        '-o', '-' // output to stdout
+        '--skip-download', 
+        '--write-auto-sub',
+        '--sub-langs', 'en',
+        '--sub-format', 'vtt'
     ]);
 
-    stream.pipe(createWriteStream(outputFile));
+    const defaultFilename = "-.en.vtt";
 
-    stream.on('end', () => console.log('✅ Download complete:', outputFile));
-    stream.on('error', (err) => console.error('❌ Error:', err));
+    stream.on('end', () => {
+        fs.rename(defaultFilename, filename, (err) => {
+            if(err) {
+                console.error('❌ Error while renaming captions file:', err);
+            } else {
+                console.log('✅ Captions download complete:')
+            }
+        });
+    });
+
+    stream.on('error', (err) => console.error('❌ Error while downloading captions:', err));
 }
 
-downloadVideo('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'video.mp4');
+downloadCaptions('https://www.youtube.com/watch?v=LYIpxnEDwp0', 'captions.vtt');
+
