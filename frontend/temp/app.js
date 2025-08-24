2dfa892cb3ba7148a1dd3200a5f4793e01162626
cjlangan <connorjameslangan@gmail.com>
2025-08-23 19:32:43 -0500

    chore: removed temporary dir on frontend

--- frontend/temp/app.js
+++
@@ -1,7 +0,0 @@
-// EXAMPLE ON HOW TO GET IMAGE THUMBNAIL FROM VIDEO ID
-// curl https://i3.ytimg.com/vi/Zj6aXVq-od8/maxresdefault.jpg --output pic.jpg
-
-// EXAMPLE ON HOW TO LINK TO VIDEO AT SPECIFC TIME
-// https://www.youtube.com/watch?v=A2HOczg0Qnw&t=74s
-
-console.log("Hello, world!")
